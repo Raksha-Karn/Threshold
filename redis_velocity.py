c3fbe5dc63406d367aec0b1cdfc595941f780ca2
Raksha Karn <karnraksha41@gmail.com>
2026-04-30 03:10:01 +0545

    Added count increment and retrieval with redis

--- redis_velocity.py
+++ redis_velocity.py
@@ -9,3 +9,17 @@ redis_client = redis.Redis(
 
 def _velocity_key(user_id: str, window: int) -> str:
     return f"velocity:txn_count:{window}s:user:{user_id}"
+
+def incr_txn_count(user_id: str, window:int = 60) -> int:
+    key = _velocity_key(user_id, window)
+    count = redis_client.incr(key)
+    if count == 1:
+        redis_client.expire(key, window)
+    return count
+
+def get_txn_count(user_id: str, window: int = 60) -> str:
+    key = _velocity_key(user_id=user_id, window=window)
+    value = redis_client.get(key)
+    if value is None:
+        return 0
+    return int(value)
