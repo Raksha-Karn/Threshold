e33a487ade2f75a5d4c935b3845ece4da66729a5
Raksha Karn <karnraksha41@gmail.com>
2026-04-30 16:34:49 +0545

    Initialized neo4j driver

---
+++ seed_graph.py
@@ -0,0 +1,11 @@
+import random
+import uuid
+import os
+from datetime import datetime, timedelta
+from neo4j import GraphDatabase
+
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+
